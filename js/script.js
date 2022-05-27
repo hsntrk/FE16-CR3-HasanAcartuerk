2802134a058f2fb6bc38cbb00239e5ded7090bf5
hsntrk <hsk@mailbox.org>
2022-05-27 15:23:19 +0200

    html rename date created js start

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var morVeÖtesi = 140;
+var duman = 20;
+var gripin = 249;
+
+var kurban = 140;
+var sebnemFerah = 20;
+var aylinAslim = 249;
+
+var manga = 140;
+var aydilge = 20;
+var cilekes = 249;
+
